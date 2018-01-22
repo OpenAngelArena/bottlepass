c61e86fce43307482048fa2288c242aa50bc4eb9
Chris Vickery <chrisinajar@gmail.com>
2018-01-21 21:24:07 -0800

    Add missing file

---
+++ src/endpoints/users.js
@@ -0,0 +1,29 @@
+const sendJSON = require('send-data/json');
+const Boom = require('boom');
+
+module.exports = Users;
+
+function Users (options) {
+  return controller;
+
+  function controller (req, res, opts, next) {
+    return controllerAsync(req, res, opts)
+      .catch(next);
+  }
+
+  async function controllerAsync (req, res, opts) {
+    var steamid = opts.splat;
+
+    try {
+      var user = await options.models.users.get(steamid);
+
+      return sendJSON(req, res, user);
+    } catch (err) {
+      console.log(err.notFound, err);
+      if (err.notFound) {
+        throw Boom.notFound('User with that id not found', err);
+      }
+      throw err;
+    }
+  }
+}
